bead21dcd731600469a57afe8fe3e313349ac0cd
Kanishka Sharma <42036722+KanishkaS@users.noreply.github.com>
2021-09-11 17:42:12 +0100

    comments supplied for MDR_test_main script

--- test_MDR/MDR_test_main.py
+++ test_MDR/MDR_test_main.py
@@ -5,10 +5,17 @@ MODEL DRIVEN REGISTRATION for iBEAt study: quantitative renal MRI
 Main script to test Model Driven Registration Library (MDR-Library)
 """
 
-
+# import the test script for your purpose
+# select from currently available test scripts below
+# T2:  MDR_test_T2
+# T2*: MDR_test_T2star
+# DWI: MDR_test_DWI
+# DTI: MDR_test_DTI
+# DCE: MDR_test_DCE
+# example import script for T2* sequence
 import MDR_test_T2star
 
-
+# main function to call MDR
 if __name__ == '__main__':
-  
+   # example use case for MDR test script using T2star sequence
     MDR_test_T2star.main()
