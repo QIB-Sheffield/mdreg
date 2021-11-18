185768c0ba4640b63c8278c244785ad539e8ed7d
JSousa-UoL <j.g.sousa@sheffield.ac.uk>
2021-11-18 17:50:09 +0000

    Incremented setup.py version to 0.0.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = "0.0.6"
+VERSION = "0.0.7"
 
 if __name__ == '__main__':
     setup(
