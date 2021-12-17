e075507519ea1ba905f6303bab303e68ed9b41a1
KanishkaS <kanishkasharma10@gmail.com>
2021-12-17 23:00:45 +0000

    Updated DCE model, test script and added MT output folder

--- tests/MDR_test_DCE.py
+++ tests/MDR_test_DCE.py
@@ -1,19 +1,16 @@
 """
-MODEL DRIVEN REGISTRATION for iBEAt study: quantitative renal MRI
-@Kanishka Sharma 2021
-Test script for DCE sequence using Model driven registration Library
+MODEL DRIVEN REGISTRATION for iBEAt study: quantitative renal MRI  
+@Kanishka Sharma  
+2021  
+Test script for DCE sequence using Model driven registration Library  
 """
 import sys
 import glob
 import os
 import numpy as np
 import importlib
-import itk
-import SimpleITK as sitk
-import pydicom
 from pathlib import Path 
 import time
-from PIL import Image
 from MDR.MDR import model_driven_registration
 from MDR.Tools import (read_DICOM_files, get_sitk_image_details_from_DICOM, 
                       sort_all_slice_files_acquisition_time, read_elastix_model_parameters,
@@ -28,7 +25,6 @@ def main():
     sequence = 'DCE'
     # number of expected slices to process (example: iBEAt study number of slice = 9)
     slices = 9    
-
     # path definition  
     # your 'os.getcwd()' path should point to your local directory containing the  MDR-Library 
     # eg: /Users/kanishkasharma/Documents/GitHub/MDR_Library
@@ -69,7 +65,7 @@ def main():
     
             # read all dicom files for the selected sequence and slice
             files, ArrayDicomiBEAt, filenameDCM = read_DICOM_files(lstFilesDCM)
-            # get sitk image parameters for registration (origin and spacing)
+            # get sitk image parameters for registration (pixel spacing)
             image_parameters = get_sitk_image_details_from_DICOM(slice_path)
             # sort slices correctly - based on acquisition time for model driven registration
             sorted_slice_files = sort_all_slice_files_acquisition_time(files)
@@ -79,24 +75,25 @@ def main():
 
 # test DCE using model driven registration
 def iBEAt_test_DCE(Elastix_Parameter_file_PATH, output_dir, sorted_slice_files, ArrayDicomiBEAt, image_parameters, AIFs_PATH, patient_folder):
-    """ Example application of MDR in renal DCE (iBEAt data)
-    
-    Args
-    ----
-    Elastix_Parameter_file_PATH (string): complete path to the Elastix parameter file to be used
-    output_dir (string): directory where results are saved
-    slice_sorted_files (list): selected slices to process using MDR - sorted according to acquisition time 
-    ArrayDicomiBEAt (numpy.ndarray): input DICOM to numpy array (unsorted)
-    image_parameters (stik tuple): distance between pixels (in mm) along each dimension.
-    AIFs_PATH (string): string with full AIF path
-    patient_folder (string): patient folder with AIFs text file
+    """ Example application of MDR in renal DCE (iBEAt data)  
 
     Description
     -----------
     This function performs model driven registration for selected DCE sequence on a single selected slice 
     and returns as output the MDR registered images, signal model fit, deformation field x, deformation field y, 
     fitted parameters Fp, Tp, Ps, Te, and the final diagnostics.
+    
+    Args
+    ----
+    Elastix_Parameter_file_PATH (string): complete path to the Elastix parameter file to be used.  
+    output_dir (string): directory where results are saved.  
+    slice_sorted_files (list): selected slices to process using MDR - sorted according to acquisition time.   
+    ArrayDicomiBEAt (numpy.ndarray): input DICOM to numpy array (unsorted).  
+    image_parameters (stik tuple): distance between pixels (in mm) along each dimension.  
+    AIFs_PATH (string): string with full AIF path.  
+    patient_folder (string): patient folder with AIFs text file.  
     """
+    
     start_computation_time = time.time()
     # define numpy array with same input shape as original DICOMs
     image_shape = np.shape(ArrayDicomiBEAt)
@@ -107,24 +104,27 @@ def iBEAt_test_DCE(Elastix_Parameter_file_PATH, output_dir, sorted_slice_files, 
         img2d = s.pixel_array
         original_images[:, :, i] = img2d
 
+    # read module file for the DCE model as string
+    full_module_name = "models.two_compartment_filtration_model_DCE"
+    # generate a module named as a string
+    model = importlib.import_module(full_module_name)
     # read signal model parameters
-    full_module_name = "models.iBEAt_DCE"
-    signal_model_parameters = read_signal_model_parameters(full_module_name,AIFs_PATH, patient_folder)
-    # read signal model parameters
+    signal_model_parameters = read_signal_model_parameters(AIFs_PATH, patient_folder)
+    # read elastix model parameters
     elastix_model_parameters = read_elastix_model_parameters(Elastix_Parameter_file_PATH, ['MaximumNumberOfIterations', 256])
     
     #Perform MDR
-    MDR_output = model_driven_registration(original_images, image_parameters, signal_model_parameters, elastix_model_parameters, precision = 1)
+    MDR_output = model_driven_registration(original_images, image_parameters, model, signal_model_parameters, elastix_model_parameters, precision = 30, function = 'main')
 
     #Export results
     export_images(MDR_output[0], output_dir +'/coregistered/MDR-registered_DCE_')
     export_images(MDR_output[1], output_dir +'/fit/fit_image_')
     export_images(MDR_output[2][:,:,0,:], output_dir +'/deformation_field/final_deformation_x_')
     export_images(MDR_output[2][:,:,1,:], output_dir +'/deformation_field/final_deformation_y_')
-    export_maps(MDR_output[3][::4], output_dir + '/fitted_parameters/Fp', np.shape(original_images))
-    export_maps(MDR_output[3][1::4], output_dir + '/fitted_parameters/Tp', np.shape(original_images))
-    export_maps(MDR_output[3][2::4], output_dir + '/fitted_parameters/Ps', np.shape(original_images))
-    export_maps(MDR_output[3][3::4], output_dir + '/fitted_parameters/Te', np.shape(original_images))
+    export_maps(MDR_output[3][0,:], output_dir + '/fitted_parameters/Fp', np.shape(original_images))
+    export_maps(MDR_output[3][1,:], output_dir + '/fitted_parameters/Tp', np.shape(original_images))
+    export_maps(MDR_output[3][2,:], output_dir + '/fitted_parameters/Ps', np.shape(original_images))
+    export_maps(MDR_output[3][3,:], output_dir + '/fitted_parameters/Te', np.shape(original_images))
     MDR_output[4].to_csv(output_dir + 'DCE_largest_deformations.csv')
 
     # Report computation times
@@ -136,15 +136,39 @@ def iBEAt_test_DCE(Elastix_Parameter_file_PATH, output_dir, sorted_slice_files, 
 
 
 ## read sequence acquisition parameter for signal modelling
-def read_signal_model_parameters(full_module_name,AIFs_PATH, patient_folder):
+def read_signal_model_parameters(AIFs_PATH, patient_folder):
    
-   # generate a module named as a string
-    MODEL = importlib.import_module(full_module_name)
-    aif, times = MODEL.load_txt(AIFs_PATH + '/' + str(patient_folder) + '/' + 'AIF__2C Filtration__Curve.txt')
+    aif, times = load_txt(AIFs_PATH + '/' + str(patient_folder) + '/' + 'AIF__2C Filtration__Curve.txt')
     aif.append(aif[-1])
     times.append(times[-1])
-    # select signal model paramters
-    signal_model_parameters = [MODEL, [aif, times]]
+    # user defined parameters
+    timepoint = 15
+    Hct = 0.45
+    # input signal model parameters
+    signal_model_parameters = [aif, times, timepoint, Hct]
     return signal_model_parameters
 
-    
+
+def load_txt(full_path_txt):
+    """ reads the AIF text file to find the AIF values and associated time-points.  
+
+        Args
+        ----
+        full_path_txt (string): file path to the AIF text file.  
+
+        Returns
+        -------
+        aif (list): arterial input function at each timepoint.  
+        time (list): corresponding timepoints at each AIF.    
+    """
+    counter_file = open(full_path_txt, 'r+')
+    content_lines = []
+    for cnt, line in enumerate(counter_file):
+        content_lines.append(line)
+    x_values_index = content_lines.index('X-values\n')
+    assert (content_lines[x_values_index+1]=='\n')
+    y_values_index = content_lines.index('Y-values\n')
+    assert (content_lines[y_values_index+1]=='\n')
+    time = list(map(lambda x: float(x), content_lines[x_values_index+2 : y_values_index-1]))
+    aif = list(map(lambda x: float(x), content_lines[y_values_index+2 :]))
+    return aif, time
