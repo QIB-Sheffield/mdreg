f0b41d037b6ba4bed5425da91a23e9cc07ea28f4
João Almeida e Sousa <53518061+JSousa-UoL@users.noreply.github.com>
2022-02-15 11:25:35 +0000

    Removed new_patch=6 in the setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ latest_major, latest_minor, latest_patch = LATEST_VERSION.split(".")
 new_major = "0"
 new_minor = "1"
 new_patch = str(int(latest_patch) + 1)  # The authors can modify this to be minor or major versions instead
-new_patch = "6"
 
 NEW_VERSION = new_major + "." + new_minor + "." + new_patch
 
