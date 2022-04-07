7a667f866c9763c6ffd68e1598c241eee5e3981c
JSousa-UoL <j.g.sousa@sheffield.ac.uk>
2022-04-07 19:32:24 +0100

    Adding README content to the package's init file. This shows the README when accessing the Github page

--- mdreg/__init__.py
+++ mdreg/__init__.py
@@ -1,2 +1,8 @@
+import os
+# Use README.md for description in Github Page
+with open(os.path.join(".", 'README.md'), encoding='utf-8') as f:
+    introduction = f.read()
+__doc__ = introduction
+
 from .main import *
 __all__ = ['MDReg', 'models']
