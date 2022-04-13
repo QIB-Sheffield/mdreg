65128ee2407b447ac410db9436025d0a380440a7
João Almeida e Sousa <53518061+JSousa-UoL@users.noreply.github.com>
2022-04-14 00:52:52 +0100

    req.close() removed

--- mdreg/__init__.py
+++ mdreg/__init__.py
@@ -7,7 +7,6 @@ if req.status_code == requests.codes.ok:
     content = base64.b64decode(req['content'])
     introduction = content.decode()
     __doc__ = introduction
-    req.close()
 
 from .main import *
 __all__ = ['MDReg', 'models']
