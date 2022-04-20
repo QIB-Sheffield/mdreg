0e5a772cd590c38d05ed571b1a8e0d23b9b8009b
João Almeida e Sousa <53518061+JSousa-UoL@users.noreply.github.com>
2022-04-20 11:17:18 +0100

    try/except in _init__ to avoid crashing if offline

--- mdreg/__init__.py
+++ mdreg/__init__.py
@@ -1,12 +1,15 @@
 import base64
 import requests
 url = 'https://api.github.com/repos/QIB-Sheffield/mdreg/contents/README.md'
-req = requests.get(url)
-if req.status_code == requests.codes.ok:
-    req = req.json()  # the response is a JSON
-    content = base64.b64decode(req['content'])
-    introduction = content.decode()
+try:
+    req = requests.get(url)
+    if req.status_code == requests.codes.ok:
+        req = req.json()  # the response is a JSON
+        content = base64.b64decode(req['content'])
+        introduction = content.decode()
     __doc__ = introduction
+except: # if there's no internet connection
+    pass
 
 from .main import *
 __all__ = ['MDReg', 'models']
