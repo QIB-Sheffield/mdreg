af4746afcbe0af78eebab256f6e8deaa7b8e41d2
Eve Shalom <79933818+EShalom@users.noreply.github.com>
2024-11-12 14:29:24 +0000

    Update getting started tutorial
    - Added fix for ukat model (requires absolute values)
    
    - Fixed progress bars to stop displaying in notebook

--- docs/examples/tutorials/plot_getting_started.py
+++ docs/examples/tutorials/plot_getting_started.py
@@ -3,13 +3,21 @@
 Getting started
 ===============================================
 
-Model-driven registration is a method to remove motion from a series of 2D or 3D images. It applies specifically to situations where a model exists that can describe the changes in signal intensity through the series. 
+Model-driven registration is a method to remove motion from a series of 2D or 
+3D images. It applies specifically to situations where a model exists that can 
+describe the changes in signal intensity through the series. 
 
-The default engine for coregistration in ``mdreg`` is the package ``itk-elastix``, but coregistration models from other packages are integrated as well (``scikit-image``, ``dipy``). 
+The default engine for coregistration in ``mdreg`` is the package 
+``itk-elastix``, but coregistration models from other packages are integrated 
+as well (``scikit-image``, ``dipy``). 
 
-For modelling, a number of generic models are built-in, but ``mdreg`` includes an interface for integrating custom-built models, or models from any other package. 
+For modelling, a number of generic models are built-in, but ``mdreg`` includes 
+an interface for integrating custom-built models, or models from any other 
+package. 
 
-This guide illustrates these different types of usage with an example use case, that of fitting the longitudinal MRI relaxation time T1 in the abdomen from a Look-Locker MRI sequence. 
+This guide illustrates these different types of usage with an example use case, 
+that of fitting the longitudinal MRI relaxation time T1 in the abdomen from a 
+Look-Locker MRI sequence. 
 
 """
 
@@ -24,7 +32,9 @@ import mdreg
 
 #%%
 # Fetch the data
-#`mdreg` includes a number of test data sets for demonstration purposes. Let's fetch the MOLLI example and use `mdreg`'s built-in plotting tools to visualise the motion:
+#`mdreg` includes a number of test data sets for demonstration purposes. Let's 
+# fetch the MOLLI example and use `mdreg`'s built-in plotting tools to 
+# visualise the motion:
 
 # fetch the data
 data = mdreg.fetch('MOLLI')
@@ -34,25 +44,34 @@ array = data['array'][:,:,0,:]
 
 # Use the built-in animation function of mdreg to visualise the motion:
 mdreg.animation(array, vmin=0, vmax=1e4, show=True)
-
 #%%
 # Using built-in models
 # =====================
 #
-# The breathing motion is clearly visible in this slice. Let's use ``mdreg`` to remove it. As a starting point, we try ``mdreg`` with default settings.
+# The breathing motion is clearly visible in this slice. Let's use ``mdreg`` to 
+# remove it. As a starting point, we try ``mdreg`` with default settings.
 
 # Perform model-driven coregistration with default settings
-coreg, defo, fit, pars = mdreg.fit(array)
+coreg, defo, fit, pars = mdreg.fit(array, verbose=0)
 
-# Visualise the results
+# # Visualise the results
 mdreg.plot_series(array, fit, coreg, vmin=0, vmax=1e4, show=True) 
 
 #%%
 # Changing the signal model
 # -------------------------
-# The default model is a constant, so the model fit (left) does not show any changes. The coregistered image (right) clearly shows the deformations, but they do not have the desired effect. This is not unexpected, because a constant model does not provide a good approximation to the changes in image contrast.
+# The default model is a constant, so the model fit (left) does not show any 
+# changes. The coregistered image (right) clearly shows the deformations, but 
+# they do not have the desired effect. This is not unexpected, because a
+# constant model does not provide a good approximation to the changes in image 
+# contrast.
 #
-# In this case we are lucky -- the appropriate model for a MOLLI sequence is `abs_exp_recovery_2p` and is included in ``mdreg``'s model library. We just need to tell ``mdreg`` which fitting function to use (``'func'``), and provide the keyword arguments required by the model. For this model these are the inversion times TI in units of sec. We define the signal model up front so it can be used again later in this script:
+# In this case we are lucky -- the appropriate model for a MOLLI sequence is 
+# `abs_exp_recovery_2p` and is included in ``mdreg``'s model library. We just 
+# need to tell ``mdreg`` which fitting function to use (``'func'``), and 
+# provide the keyword arguments required by the model. For this model these are 
+# the inversion times TI in units of sec. We define the signal model up front 
+# so it can be used again later in this script:
 
 molli = {
 
@@ -67,31 +86,38 @@ molli = {
 #Now we can run ``mdreg`` with this model and check the result again:
 
 # Perform model-driven coregistration
-coreg, defo, fit, pars = mdreg.fit(array, fit_image=molli)
+coreg, defo, fit, pars = mdreg.fit(array, fit_image=molli, verbose=0)
 
-# Visualise the results
+# # Visualise the results
 mdreg.plot_series(array, fit, coreg, vmin=0, vmax=1e4, show=True)
 
 #%%
-# This now shows essentially the desired result. The model fit (left) and the deformed image (right) are now both very similar in image contrast to the original data (middle), but with motion removed. 
+# This now shows essentially the desired result. The model fit (left) and the 
+# deformed image (right) are now both very similar in image contrast to the 
+# original data (middle), but with motion removed. 
 
 #%%
 # Using custom-built models
 # --------------------------
 #
-# In some cases the required model may not be available in the ``mdreg`` library, in which case it needs to be custom built. 
+# In some cases the required model may not be available in the ``mdreg`` 
+# library, in which case it needs to be custom built. 
 #
-# We illustrate this idea using the T1-MOLLI model from the ``ukat`` library. All that is needed in order to use it inside ``mdreg`` is to wrap it into a function that takes the image array as argument, and returns the fit to the model and the fitted model parameters. 
+# We illustrate this idea using the T1-MOLLI model from the ``ukat`` library. 
+# All that is needed in order to use it inside ``mdreg`` is to wrap it into a 
+# function that takes the image array as argument, and returns the fit to the 
+# model and the fitted model parameters. 
 
 from ukat.mapping.t1 import T1
 
-def ukat_t1_model(array, TI=None):
-    map = T1(array, TI, np.eye(4), parameters=2, multithread=False)
+def ukat_t1_model(array, TI=None, **kwargs):
+    map = T1(np.abs(array), TI, np.eye(4), parameters=2, multithread=False)
     return map.get_fit_signal(), (map.t1_map, map.m0_map)
 
 #%%
 #
-# We can now use this custom model in the same way as built-in models when we run ``mdreg``:
+# We can now use this custom model in the same way as built-in models when we 
+# run ``mdreg``:
 
 # Define the fit function and its arguments
 ukat_model = {
@@ -106,23 +132,33 @@ coreg, defo, fit, pars = mdreg.fit(array, fit_image=ukat_model)
 mdreg.plot_series(array, fit, coreg, vmin=0, vmax=1e4, show=True)
 
 #%%
-# As expected, the result is the same as before using the built-in model `abs_exp_recovery_2p` **TODO: currently NOT the case - fix bug in ukat solution**
-#
+# As expected, the result is the same as before using the built-in model 
+# `abs_exp_recovery_2p` **TODO: currently NOT the case - fix bug in ukat 
+# solution**
+
 
 #%%
 # Pixel-by-pixel fitting
 #----------------------
 #
-# In cases where the model is not available in any existing package, or the user is not prepared to import an existing package, the fit function must be written from scratch. In general, ``mdreg`` only requires that it has the same interface as the `ukat_t1_model` defined above: one argument (the image array), any number of keyword arguments, and two return values (the model fit and the fit parameters).
+# In cases where the model is not available in any existing package, or the 
+# user is not prepared to import an existing package, the fit function must be 
+# written from scratch. In general, ``mdreg`` only requires that it has the 
+# same interface as the `ukat_t1_model` defined above: one argument (the image 
+# array), any number of keyword arguments, and two return values (the model fit 
+# and the fit parameters).
 #
-# `mdreg` offers a convenient shortcut for the common scenario where a 1D signal model is applied for each pixel independently (*pixel-by-pixel fitting*). All that is needed is to define the 1D model explicitly. 
+# `mdreg` offers a convenient shortcut for the common scenario where a 1D 
+# signal model is applied for each pixel independently (*pixel-by-pixel 
+# fitting*). All that is needed is to define the 1D model explicitly. 
 #
 
-def my_pixel_model(xdata, S0, T1):
+def my_pixel_model(xdata, S0, T1, **kwargs):
     return np.abs(S0 * (1 - 2 * np.exp(-xdata/T1)))
 
 #%%
-# Optionally, one may also provide a function that derives initial values from the data and any constant initial values provided by the user.
+# Optionally, one may also provide a function that derives initial values from 
+# the data and any constant initial values provided by the user.
 #
 
 def my_pixel_model_init(xdata, ydata, p0):
@@ -131,7 +167,9 @@ def my_pixel_model_init(xdata, ydata, p0):
 
 #%%
 #
-# With these definitions in hand, a pixel model fit can be defined as a dictionary specifying the model, its parameters (xdata), and any optional arguments.
+# With these definitions in hand, a pixel model fit can be defined as a 
+# dictionary specifying the model, its parameters (xdata), and any optional 
+# arguments.
 
 
 my_pixel_fit = {
@@ -156,30 +194,45 @@ my_pixel_fit = {
 }   
 
 #%%
-#And this can be provided directly to `mdreg.fit` via the keyword argument ``fit_pixel`` - instructing ``mdreg`` to perform pixel-based fitting using the parameters defined in ``my_pixel_fit``.
+#And this can be provided directly to `mdreg.fit` via the keyword argument 
+# ``fit_pixel`` - instructing ``mdreg`` to perform pixel-based fitting using 
+# the parameters defined in ``my_pixel_fit``.
 
 # Perform model-driven coregistration with a custom pixel model
-coreg, defo, fit, pars = mdreg.fit(array, fit_pixel=my_pixel_fit)
+coreg, defo, fit, pars = mdreg.fit(array, fit_pixel=my_pixel_fit, verbose=0)
 
 # Visualise the results
 mdreg.plot_series(array, fit, coreg, vmin=0, vmax=1e4, show=True)
 
 #%%
 #
-#As expected, the result is the same as before using the built-in model `abs_exp_recovery_2p` and the ukat implementation `ukat_t1_model`. **TODO: currently NOT the case - fix bug in ukat solution**
+#As expected, the result is the same as before using the built-in model 
+# `abs_exp_recovery_2p` and the ukat implementation `ukat_t1_model`. **TODO: 
+# currently NOT the case - fix bug in ukat solution**
 
 #%%
 #Customizing the coregistration
 #==============================
 #
-#In the above examples we have not provided any detail on the coregistration itself, which means that the default in ``mdreg`` has been applied. This is the standard b-spline coregistration of elastix, but modified to use a least-squares metric rather than mutual information. The detailed default parameter settings can be found in the function `mdreg.elastix.params`.
+#In the above examples we have not provided any detail on the coregistration 
+# itself, which means that the default in ``mdreg`` has been applied. This is 
+# the standard b-spline coregistration of elastix, but modified to use a 
+# least-squares metric rather than mutual information. The detailed default 
+# parameter settings can be found in the function `mdreg.elastix.params`.
 #
-#We can try to improve the result further by customizing the coregistration model rather than using the default. This can be done either by modifying the ``elastix`` parameters, or by using another coregistration package supported by ``mdreg`` (currently only ``skimage`` available).
+#We can try to improve the result further by customizing the coregistration 
+# model rather than using the default. This can be done either by modifying the
+# ``elastix`` parameters, or by using another coregistration package supported 
+# by ``mdreg`` (currently only ``skimage`` available).
 #
-#Customizing elastix coregistration
+# Customizing elastix coregistration
 #----------------------------------
 #
-#To illustrate customizing the ``elastix`` parameters, we perform ``mdreg`` with a more fine-grained deformation field. The default coregistration uses a grid spacing of 5cm, which is relatively coarse, so we will try a finer deformation of 5mm. In order to do that, we need to provide the actual pixel spacing of the data, and modify default elastix parameters.
+# To illustrate customizing the ``elastix`` parameters, we perform ``mdreg`` 
+# with a more fine-grained deformation field. The default coregistration uses 
+# a grid spacing of 5cm, which is relatively coarse, so we will try a finer 
+# deformation of 5mm. In order to do that, we need to provide the actual pixel 
+# spacing of the data, and modify default elastix parameters.
 
 deform5mm = {
 
@@ -191,7 +244,8 @@ deform5mm = {
 }
 
 #%%
-#We run ``mdreg`` again with the correct signal model, but now using the 5mm coregistration:
+# We run ``mdreg`` again with the correct signal model, but now using the 5mm 
+# coregistration:
 
 # Perform model-driven coregistration
 coreg, defo, fit, pars = mdreg.fit(array, fit_image=molli, fit_coreg=deform5mm)
@@ -200,17 +254,27 @@ coreg, defo, fit, pars = mdreg.fit(array, fit_image=molli, fit_coreg=deform5mm)
 mdreg.plot_series(array, fit, coreg, vmin=0, vmax=1e4, show=True)
 
 #%%
-#The effect of the finer deformations is apparent, but it has not fundamentally improved the result. In fact, it has created smaller unphysical deformations that have blurred out some of the anatomical features. An example is the small cyst in the upper pole of the right kidney, which is clearly visible in the data but can no longer be seen in the model fit. The example illustrates that the grid spacing is a critical parameter and should be chosen to reflect the scale of the expected deformations. 
+# The effect of the finer deformations is apparent, but it has not 
+# fundamentally improved the result. In fact, it has created smaller unphysical
+# deformations that have blurred out some of the anatomical features. An 
+# example is the small cyst in the upper pole of the right kidney, which is 
+# clearly visible in the data but can no longer be seen in the model fit. The 
+# example illustrates that the grid spacing is a critical parameter and should 
+# be chosen to reflect the scale of the expected deformations. 
 #
-#Any coregistration method available in elastix can be used in the same way by providing a custom set of elstix parameters.
+# Any coregistration method available in elastix can be used in the same way by
+# providing a custom set of elstix parameters.
 
 #%%
 #Coregistration with ``skimage``
 #-------------------------------
 #
-#While `itk-elastix` is the default package for coregistration, ``mdreg`` also has an option to use coregistration modules from the package `scikit-image`. 
+#While `itk-elastix` is the default package for coregistration, ``mdreg`` also 
+# has an option to use coregistration modules from the package `scikit-image`. 
 #
-#For this illustration we run skimage coregistration with default parameters, except for the attachment which is increased to 30 (default=15) to allow for finer deformations.
+#For this illustration we run skimage coregistration with default parameters, 
+# except for the attachment which is increased to 30 (default=15) to allow for 
+# finer deformations.
 #
 
 attach30 = {
@@ -224,7 +288,8 @@ attach30 = {
 
 #%%
 #
-# Run ``mdreg`` again with the correct signal model, but now using the customized `skimage` coregistration:
+# Run ``mdreg`` again with the correct signal model, but now using the 
+# customized `skimage` coregistration:
 
 
 # Perform model-driven coregistration
@@ -234,7 +299,9 @@ coreg, defo, fit, pars = mdreg.fit(array, fit_image=molli, fit_coreg=attach30)
 mdreg.plot_series(array, fit, coreg, vmin=0, vmax=1e4, show=True)
 
 #%%
-# This result shows good coregistration results, in this case better preserving fine grain features such as kidney cysts in comparison to the default elastix implementation.
+# This result shows good coregistration results, in this case better preserving 
+# fine grain features such as kidney cysts in comparison to the default elastix 
+# implementation.
 
 # sphinx_gallery_start_ignore
 
